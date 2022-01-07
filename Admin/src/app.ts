438d9bcf817387af71d0f967649d6f82b5c98d2e
IwanHPrasetyo <Auxx28@gmail.com>
2022-01-07 17:56:09 +0700

    CRUD Endpoint

--- Admin/src/app.ts
+++ Admin/src/app.ts
@@ -1,15 +1,38 @@
 import * as express from 'express'
+import { Request, Response } from 'express'
 import * as cors from 'cors'
 import {createConnection} from 'typeorm'
+import {Product} from "./entity/product"
 
 createConnection().then((db)=>{
 
-    const app = express()
+const productRepository = db.getRepository(Product)
+const app = express()
+
 app.use(cors({
     origin:['http://localhost:3000','http://localhost:8080','http://localhost:4200']
 }))
 
 app.use(express.json())
+
+app.get('/api/products', async (req:Request, res:Response)=>{
+    const products = await productRepository.find()
+    res.json(products)
+})
+
+app.post('/api/products', async(req:Request, res:Response)=>{
+    const products = await productRepository.create(req.body)
+    const result = await productRepository.save(products)
+    return res.json(result)
+})
+
+app.get('/api/products/:id', async (req:Request, res:Response)=>{
+    const products =  await productRepository.findOne(req.params.id)
+    return res.send(products)
+})
+
+// app.put('')
+
 
 console.log(`Listening to port 8000`)
 app.listen(8000)
